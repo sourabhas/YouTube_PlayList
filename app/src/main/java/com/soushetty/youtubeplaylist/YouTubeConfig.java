836fec41517b2b181a7b66bda7772c2b1a8082cf
Sourabha Shetty <sou23shetty@gmail.com>
2020-05-07 16:34:27 -0400

    Update YouTubeConfig.java

--- app/src/main/java/com/soushetty/youtubeplaylist/YouTubeConfig.java
+++ app/src/main/java/com/soushetty/youtubeplaylist/YouTubeConfig.java
@@ -5,7 +5,7 @@ public class YouTubeConfig {
 
     }
 
-    private static final String api_key="AIzaSyDUT8uXudtjpKmNFq1JIfYIH9Jz2ClufB8";
+    private static final String api_key="/*Paste the generated Youtube Api Key here*/";
 
     public static String getApi_key() {
         return api_key;
